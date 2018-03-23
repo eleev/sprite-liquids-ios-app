808ce2d0710fc5e8a3339be18bc3f7e1a8233e0f
Astemir Eleev <astemireleev@gmail.com>
2018-03-23 14:50:16 +0300

    Added header files

---
+++ sprite-liquids-ios-app/SpriteLiquids.h
@@ -0,0 +1,17 @@
+//
+//  LQKSolidColorEffect.m
+//  Sprite Liquids
+//
+//  Created by Astemir Eleev on 2/8/14.
+//  Copyright (c) 2017 Astemir Eleev
+//
+
+#import "LQKCILiquidFilter.h"
+#import "LQKLiquidNode.h"
+#import "LQKLiquidParticleFactory.h"
+#import "LQKLiquidEffect.h"
+#import "LQKSolidColorEffect.h"
+
+@interface LiquidKit : NSObject
+
+@end
